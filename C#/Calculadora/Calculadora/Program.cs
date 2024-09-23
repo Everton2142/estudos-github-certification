85699e7ac8af692da2bda4362018ac3f595ac6c9
Everton2142 <evertonhenrique87@gmail.com>
2024-09-23 17:33:15 -0300

    Adição da pasta com arquivos de algoritmo em C#

---
+++ C#/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,38 @@
+﻿//Programa que funciona como uma calculadora. Ao ser executado, será necessário que o usuário informe dois números e o restante é feito automaticamente.
+
+using System;
+using System.Globalization;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            double numero1, numero2, adicao, subtracao, multiplicacao, divisao;
+
+            Console.WriteLine("Olá! Insira dois números que a calculadora irá fazer automaticamente as quatro operações matemáticas básicas e exibir os resultados.");
+
+            //Atribuição das variáveis numero1 e numero2, que serão usadas para a realização dos cálculos.
+            Console.WriteLine("Primeiro número: ");
+            numero1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Segundo número: ");
+            numero2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Realização dos cálculos das quatro operações matemáticas básicas.
+            adicao = numero1 + numero2;
+            subtracao = numero1 - numero2;
+            multiplicacao = numero1 * numero2;
+            divisao = numero1 / numero2;
+
+            //Impressão dos resultados calculados anteriormente.
+            Console.WriteLine("\nOs resulados são:");
+            Console.WriteLine($"\n{numero1.ToString("F2", CultureInfo.InvariantCulture)} + {numero2.ToString("F2", CultureInfo.InvariantCulture)} = {adicao.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{numero1.ToString("F2", CultureInfo.InvariantCulture)} - {numero2.ToString("F2", CultureInfo.InvariantCulture)} = {subtracao.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{numero1.ToString("F2", CultureInfo.InvariantCulture)} * {numero2.ToString("F2", CultureInfo.InvariantCulture)} = {multiplicacao.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{numero1.ToString("F2", CultureInfo.InvariantCulture)} / {numero2.ToString("F2", CultureInfo.InvariantCulture)} = {divisao.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ C#/Medias/Medias/Program.cs
@@ -0,0 +1,37 @@
+﻿//Este programa funciona como uma calculadora de média aritmética simples. Ao ser executado solicita que o usuário informe 4 notas e depois realiza o cálculo e apresenta os resultados.
+
+using System;
+using System.Globalization;
+
+namespace Medias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das variáveis.
+            double nota1, nota2, nota3, nota4, mediaAritmetica;
+
+            Console.WriteLine("Olá! Insira quatro notas que o programa irá fazer o cálculo de uma média aritmética simples.");
+            
+            //Atribuição dos 4 valores que serão usados para o cálculo da média, cada um numa variável.
+            Console.WriteLine("Primeira nota: ");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Terceira nota: ");
+            nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quarta nota: ");
+            nota4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Cálculo da média aritmética.
+            mediaAritmetica = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            //Impressão do resultado do cálculo feito anteriormente.
+            Console.WriteLine($"A média aritmética das quatro notas é de: {mediaAritmetica.ToString("F2", CultureInfo.InvariantCulture)}.");
+        }
+    }
+}
